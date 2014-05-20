fba49387cbe64aeb62a24f84e5c6ccdd00d8f845
Enkhtulga <enkhtulga.1@gmail.com>
2014-05-20 12:00:08 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'model.php';
+require_once 'controllers.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+$uri1 = explode("?", $uri);
+//echo $uri1;
+
+if('/index.php' == $uri){
+	list_action();
+} elseif ('/index.php/show' == $uri1[0] && isset($_GET['id']) ){
+	show_action($_GET['id']);
+} else {
+	header('Status: 404 Not found');
+	echo '<html><body><h1>Page Not Found1</h1></body></html>';
+}
+
+?>
