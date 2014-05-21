d7d283b7b31fd346f93fd62fb5a6976f4d476e51
Enkhtulga <enkhtulga.1@gmail.com>
2014-05-21 15:15:42 +0800

    Some modification

--- model.php
+++ model.php
@@ -3,7 +3,7 @@
 function open_database_connection()
 {
 	require_once 'local_settings.php';
-	$link = mysql_connect($localhost, $db_username, $db_password);
+	$link = mysql_connect($db_host, $db_username, $db_password);
 	mysql_select_db($db_name, $link);
 	mysql_query("SET NAMES 'UTF8'", $link);
 	return $link;
