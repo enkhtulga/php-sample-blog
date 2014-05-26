932edd7f8cc250709d998ad6e909d3d55a74d3de
Enkhtulga <enkhtulga.1@gmail.com>
2014-05-26 17:03:17 +0800

    I6_delete_post - Fit indent

--- templates/show.php
+++ templates/show.php
@@ -2,10 +2,10 @@
 
 <?php ob_start() ?>
 	
-<p class="blog-post-meta"><?php echo $post['date'] ?> by <?php echo $post['author'] ?>
-	<a href="/index.php/edit?id=<?php echo $post['id'] ?>" class="btn btn-warning btn-sm">  Edit post</a>
-	<a href="/index.php/delete?id=<?php echo $post['id'] ?>" class="btn btn-danger btn-sm">  Delete post</a>
-</p>
+	<p class="blog-post-meta"><?php echo $post['date'] ?> by <?php echo $post['author'] ?>
+		<a href="/index.php/edit?id=<?php echo $post['id'] ?>" class="btn btn-warning btn-sm">  Edit post</a>
+		<a href="/index.php/delete?id=<?php echo $post['id'] ?>" class="btn btn-danger btn-sm">  Delete post</a>
+	</p>
 	<div class ="body text-justify">
 		<?php echo $post['content'] ?>
 	</div>
